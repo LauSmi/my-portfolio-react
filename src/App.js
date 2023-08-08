329640f6f72bbbf40899add7060c6ba63cb3a903
laurens <lauren.smi001@gmail.com>
2023-08-08 15:28:30 -0400

    adding navbar and some css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div>
-    <h1>hello</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
